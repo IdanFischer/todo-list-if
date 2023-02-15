b6eb6f48a43f4e7731b70b085a66772d32c0cb54
Idan Fischer <Idanfischer5@gmail.com>
2023-02-15 11:11:18 -0500

    made a simple api

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+import functions from "firebase-functions"
+import { getAllTasks } from "./src/tasks.js"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/tasks', getAllTasks)
+
+// app.post('/tasks', assTask)
+
+// app.patch('/tasks/:taskId', updateTask)
+
+export const api = functions.https.onRequest(app)
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
